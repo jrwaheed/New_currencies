cfedfd0870f25a74ab73e474b77ec8a94e5b778f
jrwaheed <j.r.waheed@gmail.com>
2021-11-14 20:38:59 +0100

    force again

--- src/main/resources/static/Working_Table_Procedure.sql
+++ src/main/resources/static/Working_Table_Procedure.sql
@@ -1,77 +1,29 @@
 Delimiter $$
-Drop procedure if exists buildTargetTables$$
 CREATE PROCEDURE buildTargetTables()
 BEGIN
-	DECLARE counter INT;
-	SET counter = 0;
+DECLARE counter int default 1;
+DECLARE the_base varchar(10);
+DECLARE the_currency varchar(10);
 
-	WHILE counter <= (select count(distinct ticker)	from currency c) DO
- 		SET counter = counter + 1;
+WHILE counter <= (select count(distinct ticker) from currency c) DO
+	call targetArray;
 
- 		call targetArray;
+	set the_base = (select base from currency where id = 24);
 
- 		select @theBase:= base
-		from currency c where id = 14;
+	set the_currency = (select ticker from TArray where ID = counter);
 
-		select @theCurrency:= ticker
-		from TArray t where ID = @counter;
+	SET @DynamicSQL = 'CREATE TABLE ' + the_currency +
+	' AS (select ticker, base, time, value from currency c
+	where ticker = '+ the_currency + ' and base = ' + the_base +');';
 
-
-		SET @DynamicSQL = 'CREATE TABLE ' + @theCurrency +
-		' AS (select ticker, base, time, value from currency c
-				where ticker = '+ @theCurrency + ' and base = ' + @theBase +');';
-
-		prepare stmt from @DynamicSQL;
-
-
-		EXECUTE stmt;
-
-	END WHILE;
-
+	prepare stmt from @DynamicSQL;
+	EXECUTE stmt;
+	DEALLOCATE PREPARE stmt;
+	SET counter = counter + 1;
+END WHILE;
 END $$
-
 Delimiter ;
 
+Drop procedure if exists buildTargetTables;
 
-
-
-
-
-
-
-Delimiter //
-CREATE PROCEDURE buildTargetTables()
-BEGIN
-	declare counter int default 1;
-	DECLARE theBase varchar(10);
-	DECLARE theCurrency varchar(10);
-	WHILE counter <= (select count(distinct ticker) from currency c) DO 		
-		call targetArray;
-	
-		
-		set theBase = (base from currency where id = 24);
-
-		
-		set theCurrency = (ticker from TArray where ID = counter);
-
-		SET @DynamicSQL = 'CREATE TABLE ' + theCurrency + 
-		' AS (select ticker, base, time, value from currency c 
-		where ticker = '+ theCurrency + ' and base = ' + theBase +');';
-	
-		prepare stmt from @DynamicSQL;
-		EXECUTE stmt;
-		DEALLOCATE PREPARE stmt;
-		SET counter = counter + 1;
-	END WHILE;
-END//
-Delimiter ;
-
-
-
-Delimiter //
-CREATE PROCEDURE buildAndDeleteTargetTables()
-BEGIN
-	Drop procedure if exists buildTargetTables;
-	buildTargetTables()
-END//
-Delimiter ;
+CALL buildTargetTables(); ;
