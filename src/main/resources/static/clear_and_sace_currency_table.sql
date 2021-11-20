716d8600b711b598aa7d9546e153c618fbd5bb0f
jrwaheed <j.r.waheed@gmail.com>
2021-11-20 22:33:50 +0100

    new procedure

---
+++ src/main/resources/static/clear_and_sace_currency_table.sql
@@ -0,0 +1,13 @@
+Delimiter $$
+
+CREATE PROCEDURE clear_and_save_currency_table()
+BEGIN
+
+INSERT INTO Currency_History 
+SELECT * FROM currency;
+
+TRUNCATE TABLE currency ;
+
+END$$
+
+Delimiter ;
